13964c1da3d5e2c8d1e2430f2bc2903a8cd95c1b
pedro <pedro.barrantes@gmail.com>
2021-05-07 17:34:18 +0200

    nuevos atributos en clase prueba pedro

--- src/main/java/com/example/demo/clasePruebaPedro.java
+++ src/main/java/com/example/demo/clasePruebaPedro.java
@@ -2,4 +2,23 @@ package com.example.demo;
 
 public class clasePruebaPedro {
 
+	private int edad;
+	private String direccion;
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
 }
