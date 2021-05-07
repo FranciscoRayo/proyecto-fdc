6cdd0159faf45f97dbab5ef2b56b0cb903621995
pedro <pedro.barrantes@gmail.com>
2021-05-07 13:49:51 +0200

    primer commit con proyecto base eclipse

---
+++ src/main/java/com/example/demo/PrEclipseApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrEclipseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrEclipseApplication.class, args);
+	}
+
+}
