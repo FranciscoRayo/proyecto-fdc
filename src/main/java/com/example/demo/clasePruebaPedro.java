58c9b33273edcae13a29e879982f3a06a792aba8
Rayo <fjrayo@gmail.com>
2021-05-07 17:28:46 +0200

    Añadir atributos nuevos

--- src/main/java/com/example/demo/clasePruebaPedro.java
+++ src/main/java/com/example/demo/clasePruebaPedro.java
@@ -2,4 +2,23 @@ package com.example.demo;
 
 public class clasePruebaPedro {
 
+	private String name;
+	private String apellido;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
 }
