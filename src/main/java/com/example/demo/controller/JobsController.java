3ae191a7d939e8d1e54f5552a333be483c97ef0c
Rayo <fjrayo@gmail.com>
2021-05-19 12:29:07 +0200

    Job Controller

---
+++ src/main/java/com/example/demo/controller/JobsController.java
@@ -0,0 +1,25 @@
+
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Nombre Clase: JobsController.java Descripcin: Clase que representa al
+ * controlador de las ofertas de empleo en frontoffice Fecha: 19/05/2021
+ * Version: 1.0 Autor: Francisco Rayo
+ **/
+
+@Controller
+public class JobsController {
+	/**
+	 * Método para listar las ofertas de empleo en el front
+	 * 
+	 * @author Francisco Rayo
+	 */
+	@GetMapping("/frontoffice/ofertas-de-trabajo")
+	public String jobs() {
+		return "Jobs";
+	}
+
+}
